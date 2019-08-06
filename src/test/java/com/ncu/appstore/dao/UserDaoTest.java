1923538ed6705c5a42ddf135642e3db6596d73a0
Leo <1604270786@qq.com>
2019-08-06 22:19:46 +0800

    修改整合mybatis配置，添加el依赖，junit测试成功

--- src/test/java/com/ncu/appstore/dao/UserDaoTest.java
+++ src/test/java/com/ncu/appstore/dao/UserDaoTest.java
@@ -1,6 +1,14 @@
 package com.ncu.appstore.dao;
 
+import com.ncu.appstore.pojo.DevUser;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.Date;
 
 /**
  * @program: app-store
@@ -8,7 +16,18 @@ import org.springframework.test.context.ContextConfiguration;
  * @author: Leo
  * @create: 2019-08-06 15:38
  **/
-@ContextConfiguration(locations = {"classpath:spring-context-mybatis.xml","classpath:spring-mvc.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-context.xml","classpath:spring-mvc.xml","classpath:spring-context-mybatis.xml",
+        "classpath:spring-context-druid.xml"})
+@WebAppConfiguration("src/main/resources")
 public class UserDaoTest {
-
+    @Autowired
+    private DevUserMapper mapper;
+    @Test
+    public void testInsert(){
+        DevUser devUser = new DevUser();
+        devUser.setDevname("testAdd");
+        devUser.setCreationdate(new Date());
+        mapper.insert(devUser);
+    }
 }
