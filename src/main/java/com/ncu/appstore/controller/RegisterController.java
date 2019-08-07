fb67bd64a1845ff713aeb8ae1632394d69461f7f
shelbaydo <38807661+shelbaydo@users.noreply.github.com>
2019-08-07 20:32:17 +0800

    前台用户注册功能完成

--- src/main/java/com/ncu/appstore/controller/RegisterController.java
+++ src/main/java/com/ncu/appstore/controller/RegisterController.java
@@ -1,5 +1,6 @@
 package com.ncu.appstore.controller;
 
+import com.ncu.appstore.common.CodeCaptchaServlet;
 import com.ncu.appstore.pojo.DevUser;
 import com.ncu.appstore.service.DevUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -48,6 +51,31 @@ public class RegisterController extends BaseController {
         }
         return map;
     }
+    /**
+     * 判断验证码是否正确
+     *
+     * @param code
+     * @return
+     */
+    @RequestMapping("/checkCode")
+    @ResponseBody
+    public Map<String, Object> checkCode( @RequestParam(value = "code", required = false) String code) {
+
+        Map map = new HashMap<String, Object>();
+        ServletRequestAttributes attrs = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        String vcode = (String) attrs.getRequest().getSession().getAttribute(CodeCaptchaServlet.VERCODE_KEY);
+
+        if (code.equals(vcode)) {
+            //验证码正确
+            map.put("message", "success");
+        } else {
+            //验证码错误
+            map.put("message", "fail");
+        }
+
+        return map;
+    }
+
     @RequestMapping("/register")
     public String register(Model model, @RequestParam(value = "devcode" ,required = true) String devcode,
                            @RequestParam(value = "devname" ,required = true) String devname,
---
+++ src/main/webapp/register/registerSuccess.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: LICHUNYAN
+  Date: 2019/8/7
+  Time: 15:37
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    恭喜注册成功
+    <a href="../jsp/index.jsp">返回首页</a>
+</body>
+</html>
